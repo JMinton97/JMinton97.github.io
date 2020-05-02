d82bba30bacb86b00e8737725dae0c56f9a1a5ab
Joshua Minton <joshuamntn@gmail.com>
2020-05-02 16:45:57 +0100

    Added popup

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-console.log("Hello, World!")
-const div = document.createElement('div')
-div.classList.add('photo')
 const photoGrid = document.querySelector('.photo_grid')
-div.setAttribute('background-image', 'url(\'me.jpg\')');
-div.setAttribute('background-color', 'blue');
-div.style.backgroundImage = 'url(\'me.jpg\')'
-photoGrid.appendChild(div)
-photoGrid.appendChild(div)
+
+for (var i = 0; i < 12; i++) {
+	const div = document.createElement('div')
+	div.classList.add('photo')
+	div.classList.add('shadowed')
+	div.setAttribute('background-color', 'blue')
+	div.style.opacity = 1;
+
+	div.addEventListener('click', () => {
+		// console.log(square.style.opacity)
+		const popup = document.querySelector('.popup')
+  		popup.style.visibility = 'visible'
+  		const body = document.querySelector('body')
+  		body.classList.add('noScroll')
+  		div.classList.add('shadowed')
+	})
+
+	photoGrid.appendChild(div)
+	photoGrid.appendChild(div)
+}
